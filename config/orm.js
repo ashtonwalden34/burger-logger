5dd85ad11ca1e84532fcbca0fd7844a2196dc062
Ashton Walden <ashtonwalden@icloud.com>
2020-07-19 22:43:37 -0600

    attempted orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,28 @@
 // requires connection from 'connection.js'
 const connection = require("./connection");
 
+// object to hold all MySQL functions
+var orm = {
+    selectAll: function() {
+        var queryString = "SELECT * FROM burger";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(colToSearch) {
+        var queryString = "INSERT INTO burger";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(colToUpdate, newVal, colToSearch, oldVal) {
+        // may be "UPDATE burger SET ? WHERE ?"
+        var queryString = "UPDATE burger SET ?? = ? WHERE ?? = ?"
+        connection.query(queryString, [colToUpdate, newVal, colToSearch, oldVal], function(err, result) {
+            if (err) throw err
+            console.log(result)
+        });
+    }
+};
