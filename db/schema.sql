9199a80d05a5d4685e517fab3c006ac1fda46d6f
Ashton Walden <ashtonwalden@icloud.com>
2020-07-19 18:02:52 -0600

    filled in schema and seeds sql files

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Deletes existing databse named 'burger_db'
+DROP DATABASE IF EXISTS burger_logger_db;
+-- Creates and uses database named 'burger_db'
+CREATE DATABASE burger_logger_db;
+USE burger_logger_db;
+
+-- creates burger table to hold id, name, and devoured status
+CREATE TABLE burger (
+        burger_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        burger_name VARCHAR (30) NOT NULL,
+        devoured BOOLEAN NOT NULL
+)
+
+-- selects all items from burger table
+SELECT * FROM burger;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- uses burger database
+USE burger_logger_db;
+
+-- Inserts dummy data into burger table
+INSERT INTO burger (burger_name, devoured)
+VALUES ("burger1", 1), ("burger2", 0), ("burger3", 1)
