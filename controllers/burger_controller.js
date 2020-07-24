4e2fec4e2dae963d10b3ae350987413d9f702e25
Ashton Walden <ashtonwalden@icloud.com>
2020-07-24 00:44:12 -0600

    cleaned up code

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,22 +1,3 @@
-// requires express and burger.js file
-const express = require("express");
-const burgerJS = ruquire("burger.js");
-
-// code to create "router"
-var router = express.Router();
-
-// get request for index page
-router.get("/home", function(req, res) {
-
-})
-
-// export the "router"
-module.exports("router");
-
-
-
-
-
 // require express
 var express = require("express";
 // variable to hold router
