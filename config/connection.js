abb1eb8ef9b2a652fa38ef6e19fc4b4c29f89a63
Ashton Walden <ashtonwalden@icloud.com>
2020-07-30 15:28:00 -0600

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,27 @@
 // requires MySQL
 const mysql = require("mysql");
+var connection;
 
-// sets up connection to database
-var connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306,
-    user: "root",
-    password: "password",
-    // selects burger logger database
-    database: "burger_logger_db"
-});
+// // sets up connection to database
+// var connection = mysql.createConnection({
+//     host: "localhost", 
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     // selects burger logger database
+//     database: "burger_logger_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: burger_logger_db
+    });
+};
 
 // connects to the database
 connection.connect(function(err) {
