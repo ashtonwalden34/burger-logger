417a97c811dbf0321cb99e6eb12315738f1ae8ab
Ashton Walden <ashtonwalden@icloud.com>
2020-08-03 18:10:55 -0600

    fixed syntax

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,6 +14,7 @@ router.get("/", function(req, res) {
             burgers: data
         };
         // console.log(hbsObject);
+        // console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
